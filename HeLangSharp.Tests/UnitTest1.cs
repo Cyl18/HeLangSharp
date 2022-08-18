82c0218bd880e8591c77f9416e4316842f123d70
Cyl18 <cyl18a@gmail.com>
2022-08-18 09:07:29 +0800

    Add project files.

---
+++ HeLangSharp.Tests/UnitTest1.cs
@@ -0,0 +1,74 @@
+using HeLangSharp;
+using System.Collections;
+using System.Text;
+using static HeLangSharp.U8Base;
+
+namespace HeLangSharp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInit()
+        {
+            u8 a = dian | 1 | 2 | le;
+
+            CollectionAssert.AreEqual(a.nums.ToArray(), new nint[] { 1, 2 });
+        }
+
+        [TestMethod]
+        public void TestIncrement()
+        {
+            u8 a = dian | 1 | 2 | le;
+            a++;
+            CollectionAssert.AreEqual(a.nums.ToArray(), new nint[] { 2, 3 });
+        }
+
+        [TestMethod]
+        public void TestElementOperations()
+        {
+            u8 a = dian | 1 | 2 | 3 | 4 | 5 | le;
+
+            Assert.AreEqual(a[1], 1);
+
+            a[5] = 0;
+            Assert.AreEqual(a[5], 0);
+
+            a[dian | 1 | 3 | le] = 0;
+            CollectionAssert.AreEqual(a.nums.ToArray(), new nint[] { 0, 2, 0, 4, 0 });
+
+            a[0] = 0;
+            CollectionAssert.AreEqual(a.nums.ToArray(), new nint[] { 0, 0, 0, 0, 0 });
+        }
+
+        [TestMethod]
+        public void TestPrint()
+        {
+            var original = Console.Out;
+            var sb = new StringBuilder();
+            var writer = new StringWriter(sb);
+            Console.SetOut(writer);
+
+            u8 a = dian | 1 | 2 | 3 | 4 | 5 | le;
+
+            Print(a);
+
+            u8 hw = dian | 72 | 101 | 108 | 108 | 111 | 44 | 32 | 119 | 111 | 114 | 108 | 100 | 33 | le;
+            SPrint(hw);
+
+            Assert.AreEqual(sb.ToString(), "1 | 2 | 3 | 4 | 5" + Environment.NewLine +
+                                           "Hello, world!" + Environment.NewLine);
+
+            Console.SetOut(original);
+        }
+
+        [TestMethod]
+        public void TestSpan()
+        {
+            u8 a = stackalloc nint[10];
+            a[0] = 1;
+            
+            CollectionAssert.AreEqual(a.nums.ToArray(), Enumerable.Repeat((nint)1, 10).ToArray());
+        }
+    }
+}
---
+++ HeLangSharp/U8.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Immutable;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace HeLangSharp
+{
+    public class U8Base
+    {
+        public static readonly U8Start 典 = new U8Start();
+        public static readonly U8Start dian = new U8Start();
+        public static readonly U8End 乐 = new U8End();
+        public static readonly U8End le = new U8End();
+
+        public static void Test5G()
+        {
+            // https://github.com/shaokeyibb/HeLangKotlinScriptImpl/blob/bc4a379e90bfbcb5f5fa619a5d1cd5137e7ac764/src/main/kotlin/io/hikarilan/helangkotlinscriptimpl/SpeedTester.kt
+            // https://github.com/kifuan/helang/blob/219c3f5da2031154749996d322780941b5d5558b/helang/speed_tester.py
+
+            throw new NotImplementedException();
+        }
+
+        public static void SPrint(u8 a)
+        {
+            for (int i = 0; i < a.nums.Length; i++)
+            {
+                var num = a.nums[i];
+                Console.Write((char) num);
+            }
+            Console.WriteLine();
+        }
+
+        public static void Print(u8 a)
+        {
+            for (int i = 0; i < a.nums.Length; i++)
+            {
+                var num = a.nums[i];
+                Console.Write(i == 0 ? $"{num}" : $" | {num}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public ref struct u8
+    {
+        public Span<nint> nums { get; set; }
+
+        internal u8(Span<nint> nums)
+        {
+            this.nums = nums;
+        }
+
+        public static u8 operator ++(u8 a)
+        {
+            for (var i = 0; i < a.nums.Length; i++)
+            {
+                a.nums[i]++;
+            }
+
+            return a;
+        }
+
+        public static implicit operator u8(Span<nint> a)
+        {
+            return new u8(a);
+        }
+
+        public nint this[u8 u]
+        {
+            set
+            {
+                foreach (var num in u.nums)
+                {
+                    nums[(int)num - 1] = value;
+                }
+            }
+        }
+
+        public nint this[nint v]
+        {
+            get
+            {
+                return nums[(int) v - 1];
+            }
+            set
+            {
+                if (v == 0)
+                {
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        nums[i] = value;
+                    }
+                }
+                else
+                {
+                    nums[(int)v - 1] = value;
+                }
+            }
+        }
+
+    }
+
+    public struct U8Start
+    {
+        public static U8Builder operator |(U8Start b, nint i)
+        {
+            return new U8Builder(i);
+        }
+    }
+
+    public struct U8End
+    {
+
+    }
+
+    public class U8Builder
+    {
+        List<nint> Nums = new();
+
+        public U8Builder(nint startNum)
+        {
+            Nums.Add(startNum);
+        }
+
+        public void Add(nint num)
+        {
+            Nums.Add(num);
+        }
+
+        public u8 Build()
+        { 
+            //u8 a = stackalloc int[10];
+            return new u8(CollectionsMarshal.AsSpan(Nums));
+        }
+
+        public static u8 operator |(U8Builder b, U8End e)
+        {
+            return b.Build();
+        }
+
+        public static U8Builder operator |(U8Builder b, nint i)
+        {
+            b.Add(i);
+            return b;
+        }
+    }
+}
